8162bc4b6cf2fbb9cb3731a13cb306ccdb19d8d1
Hoang Anh Tuan <tuan.ha@altplus.com.vn>
2017-04-28 14:16:17 +0700

    fix btl

---
+++ BTL/js/myscript.js
@@ -0,0 +1,36 @@
+$( document ).ready( function() {
+    // js header nav
+    $("button#icon_menu").click(function(e) {
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('#page #ovelay-bg').css('display','none');
+            $("#menu").removeClass('active');
+        } else {
+            $(this).addClass('active');
+            $('#page #ovelay-bg').css('display','block');
+            $("#menu").addClass('active');
+        }
+    })
+    $('#page #ovelay-bg').click(function(e) {
+        $('#page #ovelay-bg').css('display','none');
+        $("#icon_menu").removeClass('active');
+        $("#menu").removeClass('active');
+    });
+    $(window).scroll(function(event){
+        if($("#icon_menu").hasClass('active')){
+            $("#icon_menu").removeClass('active');
+            $('#page #ovelay-bg').css('display','none');
+            $("#menu").removeClass('active');
+        }
+    });
+    $("#menu span.rd-navbar-submenu-toggle").click(function(e) {
+        if($(this).parents('li').hasClass('opened')){
+            $(this).parents('li').removeClass('opened');
+        } else {
+            $('#menu').find('li').removeClass('opened');
+            $(this).parents('li').addClass('opened');
+        }
+    });
+    // end
+
+});
