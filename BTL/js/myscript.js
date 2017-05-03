ffaf09c681be4c2301ce6065b9c2c4fc519d2857
Hoang Anh Tuan <tuan.ha@altplus.com.vn>
2017-05-03 16:38:14 +0700

    update header oke

--- BTL/js/myscript.js
+++ BTL/js/myscript.js
@@ -1,28 +1,55 @@
 $( document ).ready( function() {
     // js header nav
-    $("button#icon_menu").click(function(e) {
-        if($(this).hasClass('active')){
+    $("#icon-contacts").click(function(event) {
+        if($(this).hasClass('active')) {
             $(this).removeClass('active');
-            $('#page #ovelay-bg').css('display','none');
-            $("#menu").removeClass('active');
+            $("#contacts").removeClass('active');
         } else {
             $(this).addClass('active');
-            $('#page #ovelay-bg').css('display','block');
-            $("#menu").addClass('active');
+            $("#contacts").addClass('active');
+            $("#icon_menu").removeClass('active');
+            // $('#page #ovelay-bg').css('display','none');
+            $("#menu").removeClass('active');
         }
-    })
-    $('#page #ovelay-bg').click(function(e) {
-        $('#page #ovelay-bg').css('display','none');
+    });
+    $(".page-content").click(function(event) {
+        $("#icon-contacts").removeClass('active');
+        $("#contacts").removeClass('active');
         $("#icon_menu").removeClass('active');
         $("#menu").removeClass('active');
     });
+
+    $("button#icon_menu").click(function(e) {
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            // $('#page #ovelay-bg').css('display','none');
+            $("#menu").removeClass('active');
+        } else {
+            $(this).addClass('active');
+            // $('#page #ovelay-bg').css('display','block');
+            $("#menu").addClass('active');
+            $("#icon-contacts").removeClass('active');
+            $("#contacts").removeClass('active');
+        }
+    })
+    // $('#page #ovelay-bg').click(function(e) {
+    //     $('#page #ovelay-bg').css('display','none');
+    //     $("#icon_menu").removeClass('active');
+    //     $("#menu").removeClass('active');
+    // });
+
     $(window).scroll(function(event){
         if($("#icon_menu").hasClass('active')){ 
             $("#icon_menu").removeClass('active');
-            $('#page #ovelay-bg').css('display','none');
+            // $('#page #ovelay-bg').css('display','none');
             $("#menu").removeClass('active');
         }
+        if($("#icon-contacts").hasClass('active')) {
+            $("#icon-contacts").removeClass('active');
+            $("#contacts").removeClass('active');
+        }
     });
+
     $("#menu span.rd-navbar-submenu-toggle").click(function(e) {
         if($(this).parents('li').hasClass('opened')){
             $(this).parents('li').removeClass('opened');
